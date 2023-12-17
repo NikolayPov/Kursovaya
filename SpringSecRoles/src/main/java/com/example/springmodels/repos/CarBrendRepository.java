92ac4f6a8466fbc35e71607ead14fb860aa30a84
NikolayPoverin <isip_n.o.poverin@mpt.ru>
2023-12-17 19:24:36 +0300

    first commit

---
+++ SpringSecRoles/src/main/java/com/example/springmodels/repos/CarBrendRepository.java
@@ -0,0 +1,6 @@
+package com.example.springmodels.repos;
+import com.example.springmodels.models.CarBrend;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface CarBrendRepository extends JpaRepository<CarBrend, Integer> {
+
+}
---
+++ SpringSecRoles/src/test/java/com/example/springmodels/SpringModelsApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.springmodels;
+
+import com.example.springmodels.models.Service;
+import com.example.springmodels.repos.ServiceRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+class SpringModelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
